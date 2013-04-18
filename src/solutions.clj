b6b746b82689e21abc66e73acf58e40a6c50b0eb
Ramesh Thulasiram <ramesh.thulasiram@gmail.com>
2013-04-18 12:44:02 -0700

    added 2 more solutions

--- src/solutions.clj
+++ src/solutions.clj
@@ -9,12 +9,20 @@
   (reduce + 
     (filter #(> 1000 %) (distinct (concat (multiples 3) (multiples 5)))))))
 
+(defn fibnacci-sum-even-num<n [n] 
+  (loop [[a b c] [0 1 0]] (if (> b n) c (recur [b (+ a b) (if (even? b) (+ c b) c)]))))
+
+(defn prime? [n] (not (reduce #(or %1 (zero? (mod n %2))) (concat '(false) (range (int (Math/sqrt n)) 1 -1)))))
+
+(defn largest-prime-factor-bad [n] (loop [x (int (Math/sqrt n))] (if (and (zero? (mod n x)) (prime? x)) x (recur (dec x))))) 
+;(int  (Math/sqrt 600851475143))
 
 
 
 
 (defn main []
-  (println (sum-multiples-3-5<1000)))
+;	(println (map #(do (println % " " (prime? %))) (range 100))))
+ (println (largest-prime-factor-bad 600851475143)))
 
 (main)
 
