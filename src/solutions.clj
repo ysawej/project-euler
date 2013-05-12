2be0794090f27b770a106760c37ae3ee944b0b34
Ramesh Thulasiram <ramesh.thulasiram@gmail.com>
2013-05-11 23:34:58 -0700

    adding problem 13 files & bug fixes (sieve, make-lean, ) & code formatting

--- src/solutions.clj
+++ src/solutions.clj
@@ -3,28 +3,39 @@
 (defn mysolutions [] "My solutions to project euler in clojure")
 
 ;; Problem # 1 what is the sum of all multiples of 3 & 5 <1000?
-(defn sum-multiples-3-5<1000 [] 
-  (letfn [(multiples [n] 
-            (map #(* n %) (range (/ 1000 n))))]
-  (reduce + 
-    (filter #(> 1000 %) (distinct (concat (multiples 3) (multiples 5)))))))
+(defn sum-multiples-of-3-and-5-lt-1000 [] 
+  (letfn [ (multiples [n] (map #(* n %) (range (/ 1000 n))))] 
+    (reduce + (filter #(> 1000 %) (distinct (concat (multiples 3) (multiples 5)))))))
 
-(defn sum-3&5*<1000 []
-(loop [x3 3 x5 5 sigma 0 ] (if-not (or (< x3 1000) (< x5 1000)) sigma (cond (> x3 x5)  (recur x3 (+ x5 5) (+ sigma x5)) (= x3 x5) (recur (+ x3 3) (+ x5 5) (+ sigma x3)) (< x3 x5)
-(recur (+ x3 3) x5 (+ sigma x3))))))
+(defn sum-multiples-of-3-and-5-lt-1000-better [] 
+(loop [x3 3 x5 5 sigma 0 ] 
+  (if (or (< x3 1000) (< x5 1000)) 
+    (cond 
+      (> x3 x5) (recur x3 (+ x5 5) (+ sigma x5)) 
+      (= x3 x5) (recur (+ x3 3) (+ x5 5) (+ sigma x3)) 
+      (< x3 x5) (recur (+ x3 3) x5 (+ sigma x3)))
+    sigma)))
 
 ;; Problem # 2
-(defn fibnacci-sum-even-num<n [n] 
+(defn sum-even-valued-fibonacci-terms-lt-n [n] 
   (loop [[a b c] [0 1 0]] (if (> b n) c (recur [b (+ a b) (if (even? b) (+ c b) c)]))))
 
 ;; Problem # 3
 (defn prime? [n] (not (reduce #(or %1 (zero? (mod n %2))) (concat '(false) (range (int (Math/sqrt n)) 1 -1)))))
 
-(defn largest-prime-factor-bad [n] (loop [x (int (Math/sqrt n))] (if (and (zero? (mod n x)) (prime? x)) x (recur (dec x))))) 
+(defn largest-prime-factor-bad [n] 
+  (loop [x (int (Math/sqrt n))] (if (and (zero? (mod n x)) (prime? x)) x (recur (dec x))))) 
 ;(int  (Math/sqrt 600851475143))
 
 ;; Problem # 4
-(defn palindrome? [number] (let [ind-digits (fn [number] (loop [x [] number number] (if (zero? number) x (recur (cons (mod number 10) x) (int (/ number 10)))))) digits (ind-digits number)]  (= (reverse digits) digits)))
+(defn palindrome? [number] 
+  (let [ind-digits (fn [number] 
+                     (loop [x [] number number] 
+                       (if (zero? number) 
+                         x 
+                         (recur (cons (mod number 10) x) (int (/ number 10))))))
+        digits (ind-digits number)]
+    (= (reverse digits) digits)))
 
 (defn three? [number x] (and (zero? (mod number x)) (>= 999 x 100)))
 
@@ -46,9 +57,9 @@
 
 ; Problem # 7
 (defn sieve-primes [n] 
-(let [arr (vec (repeat n true)) ];mymap (java.util.HashMap. {:N 1 :prime 2}) ] 
+(let [arr (vec (repeat (dec n) true)) ];mymap (java.util.HashMap. {:N 1 :prime 2}) ] 
 (loop [x 2 sieve arr i 0]
-(if-not (< (* x x) n) sieve (if-not (sieve i) (recur (inc x) sieve (inc i)) (recur (inc x) (apply assoc sieve (interleave (range (+ i x) n x) (repeat false))) (inc i)))))))
+(if-not (<= (* x x) n) sieve (if-not (sieve i) (recur (inc x) sieve (inc i)) (recur (inc x) (apply assoc sieve (interleave (range (+ i x) n x) (repeat false))) (inc i)))))))
 
 (defn last-prime [n] (let [sieve (sieve-primes n)] 
 (loop [cnt 0 x 0 prime 2] (if (>= cnt n) [x prime] (if (sieve cnt) (recur (inc cnt) (inc x) (+ cnt 2)) (recur (inc cnt) x prime) )))))
@@ -68,7 +79,14 @@
 ( let [doublet (filter pyth-1k-sum? (range 1 667)) triplet (conj doublet (- 1000 (apply + doublet)))] (apply * triplet))
 
 ;; Problem # 10
-(defn sum-primes [n] (let [sieve (sieve-primes n)] (loop [sum 0 index 0] (if (>= index (- n 2)) sum (if (sieve index) (recur (+ sum index 2) (inc index)) (recur sum (inc index)))))))
+(defn sum-primes [n] 
+  (let [sieve (sieve-primes n)] 
+    (loop [sum 0 index 0] 
+      (if (>= index (- n 2)) 
+        sum 
+        (if (sieve index) 
+          (recur (+ sum index 2) (inc index)) 
+          (recur sum (inc index)))))))
 
 ;(sum-primes 2000000)
 
@@ -86,53 +104,92 @@
 )))))
 
 ;; Problem # 12 
-;;
-;;
-;;
-;;
-;;
-;;
-;
 (defn int-sqrt [x] (int (Math/sqrt x)))
-(defn power-exact-divide [n x] (if (some zero? [n x]) 0 (if (zero? (mod n x)) (+ 1 (power-exact-divide (/ n x) x)) 0)))
 
-(defn prime-factors-orig [n] (loop [sieve (sieve-primes (int-sqrt n)) x 2 res (sorted-map) res2 (sorted-map n 1)] 
-                          (do (println sieve)
-                          (if (empty? sieve) 
-                            (merge res (apply dissoc res2
-                                              (filter 
-                                                #(some zero? (map mod (repeat %) (keys res))) 
-                                                      (keys res2))))
-                            (if (and (first sieve) (zero? (mod n x))) 
-                              (recur (rest sieve) (inc x) (assoc res x (power-exact-divide n x)) (assoc res2 (/ n x) (power-exact-divide n (/ n x))))
-                              (recur (rest sieve) (inc x) res res2))))))
-(defn prime-factors [n] (loop [sieve (butlast (sieve-primes n)) x 2 res (sorted-map)]
-                          (if (empty? sieve) res
-                            (if (and (first sieve) (zero? (mod n x))) (recur (rest sieve) (inc x) (assoc res x (power-exact-divide n x))) (recur (rest sieve) (inc x) res)))))
+(defn power-exact-divide [n x] 
+  (if (some zero? [n x]) 0 (if (zero? (mod n x)) (+ 1 (power-exact-divide (/ n x) x)) 0)))
 
-(defn power-set-enumerate [myset] (let [cnts (map-indexed vector (map #(reduce * (repeat % 2)) (range (count myset))))]
-                                    (for [x (range (reduce * (repeat (count myset) 2)))] (map #(nth myset (first %)) (filter #(pos? (bit-and x (second %))) cnts)))))
+; Assumes primes contain only primes and they are all < x
+(defn make-lean-int [x primes]
+  ;(do (println "x & primes are " x " " primes)
+  (let [x2 (reduce #(/ %1 %2) (concat [x] (filter #(zero? (mod x %)) primes)))] 
+    (if (= x2 x) x (make-lean-int x2 primes))));)
 
-(defn nth-trinum-factors-count [n]  (let [prime-facs (apply merge-with + (map prime-factors (if (odd? n) [(-> n inc (/ 2)) n] [(/ n 2) (inc n)])))]
-                                      (let [the-sum
-                                      (loop [sum 0 pow-set (power-set-enumerate (keys prime-facs))] 
-                                        (if-let [fst (first pow-set)] 
-                                          (if (empty? fst) (recur (inc sum) (rest pow-set)) 
-                                            (recur (+ sum (reduce * (map #(prime-facs %) fst))) (rest pow-set))) 
-                                          sum))] the-sum)))
+(println (make-lean-int 170 [2 5]))
+
+; Good one, goes till sqrt n,  
+(defn prime-factors-orig [n] 
+  (loop [sieve (sieve-primes (int-sqrt n)) x 2 res (sorted-map) res2 (sorted-map n 1)] 
+    (do (println sieve)
+        (if (empty? sieve) 
+          (merge res (apply dissoc res2
+                            (filter 
+                              #(some zero? (map mod (repeat %) (keys res))) 
+                              (keys res2))))
+          (if (and (first sieve) (zero? (mod n x))) 
+            (recur (rest sieve) (inc x) (assoc res x (power-exact-divide n x)) (assoc res2 (/ n x) 1))
+            (recur (rest sieve) (inc x) res res2))))))
+
+(defn prime-factors-orig2 [n] 
+  (loop [sieve (butlast (sieve-primes n)) x 2 res (sorted-map)]
+    (if (empty? sieve) 
+      res
+      (if (and (first sieve) (zero? (mod n x))) 
+        (recur (rest sieve) (inc x) (assoc res x (power-exact-divide n x))) 
+        (recur (rest sieve) (inc x) res)))))
+
+(defn prime-factors [n] 
+  (loop [sieve (sieve-primes (int-sqrt n)) x 2 res (sorted-map) res2 (sorted-map n 1)] 
+    ;(do (println "so far prime factors " res " for n" n)
+    (if-not (empty? sieve) 
+      (if (and (first sieve) (zero? (mod n x)))
+        (recur (rest sieve) (inc x) (assoc res x (power-exact-divide n x)) (assoc res2 (/ n x) 1))
+        (recur (rest sieve) (inc x) res res2))
+      (dissoc 
+        (merge 
+          (reduce #(apply assoc %1 %2) {} (map #(vec [ (make-lean-int % (keys res)) 1 ]) (keys res2))) 
+          res) 
+        1))));)
+
+
+(defn power-set-enumerate [myset] 
+  (let [myvec (vec myset) 
+        cnts (map-indexed vector (map #(reduce * (repeat % 2)) (range (count myvec))))]
+    (for [x (range (reduce * (repeat (count myvec) 2)))] 
+      (map #(nth myvec (first %)) (filter #(pos? (bit-and x (second %))) cnts)))))
+
+(defn nth-trinum-factors-count [n]  
+  (let [prime-facs (apply merge-with + 
+                          (map prime-factors (if (odd? n) [(-> n inc (/ 2)) n] [(/ n 2) (inc n)])))]
+    ;(let [the-sum
+          (loop [sum 0 pow-set (power-set-enumerate (keys prime-facs))] 
+            (if-let [fst (first pow-set)] 
+              (if (empty? fst) (recur (inc sum) (rest pow-set)) 
+                (recur (+ sum (reduce * (map #(prime-facs %) fst))) (rest pow-set))) 
+              sum))
+            ))
+    ;      ] the-sum)))
 ;                                      (do (println n " and its prime factors " prime-facs " and the total-num of factors " the-sum) the-sum))))
+;
 
-
+; Problem # 13
+;
+(def nums (slurp "data/problem13"))
+(defn rshift-10-digs [n] (long (/ n 10000000000)))
+(defn str-arr-to-num [coll] (reduce #(+ (* %1 10) %2) (map #(- (int %) (int \0)) coll)))
+(println "The answer to problem#13 is first 10 digits of "
+         (+ (reduce + (map (comp str-arr-to-num first) (map #(partition 10 %) (clojure.string/split nums #"\s"))))
+            (rshift-10-digs (reduce + (map (comp str-arr-to-num second) (map #(partition 10 %) (clojure.string/split nums #"\s")))))))
 
 
 (defn main []
 ;        (nth-trinum-factors-count 12375))
 ;(comment
-        (time (loop [x 10 y (nth-trinum-factors-count x)] 
+        (time (loop [x 10 y (nth-trinum-factors-count x)]
           (if (< 500 y) 
             (println x " with " y " factors") 
             (recur (inc x) (nth-trinum-factors-count (inc x))))) ))
-;  (println "prime factors of 170 " (prime-factors 170)))
+  ;(println "prime factors of 170 " (prime-factors 170)))
 (comment
 	(time (println "110000 primes, 10001 prime " (mth-prime 110000 10001)))
 	(println "1000000 primes" (last-prime 1000000))
